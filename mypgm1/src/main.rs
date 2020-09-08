00f1e8009e8699b3286dd7e6b2b109261d48c0f6
Paige <paigepeck@hotmail.com>
2020-09-07 21:12:35 -0400

    Minor changes to test case

--- mypgm1/src/main.rs
+++ mypgm1/src/main.rs
@@ -7,10 +7,10 @@ fn main() {
     let nodes = s.split(",").collect::<Vec<&str>>();
 
     //Call write_to_file function using the Vec(&str) that was parsed from the command line arguments
-    write_to_file(nodes);
+    write_to_file(nodes, "peck_graph.dot");
 }
 
-fn write_to_file(node_names: Vec<&str>) {
+fn write_to_file(node_names: Vec<&str>, file_name: &str) {
 	let mut accept_state = String::new();
 	let mut start_state = String::new();
 	let mut previous_state = String::new();
@@ -25,7 +25,7 @@ fn write_to_file(node_names: Vec<&str>) {
     start_state.push_str(node_names[0]);
 
     //Create file, add in Dot code
-    let mut file = std::fs::File::create("peck_graph.dot").expect("create failed");
+    let mut file = std::fs::File::create(file_name).expect("create failed");
     file.write_all("digraph finite_state_machine {".as_bytes()).expect("write failed");
     file.write_all("\nrankdir=LR;".as_bytes()).expect("write failed");
     file.write_all(accept_state.as_bytes()).expect("write failed");
@@ -56,13 +56,14 @@ fn write_to_file(node_names: Vec<&str>) {
 fn test_write_to_file() {
 	let test_nodes = "a,b,c,d,e,f".split(",").collect::<Vec<&str>>();
 
-	write_to_file(test_nodes);
+	write_to_file(test_nodes, "test_file.dot");
 
 	let file_contents = fs::read_to_string("peck_graph.dot").expect("Something went wrong reading the file");
 
 	let mut test_case = String::new();
 	test_case.push_str("digraph finite_state_machine {\nrankdir=LR;\nnode [shape = doublecircle]; f\nnode [shape = point ]; qi\nnode [shape = circle]\nqi -> a\na -> b\nb -> c\nc -> d\nd -> e\ne -> f\n}");
 
+	fs::remove_file("test_file.dot").expect("Something went wrong removing the file");
 	assert_eq!(file_contents, test_case);
 }
 
